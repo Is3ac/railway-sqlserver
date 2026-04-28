0debba83168ea690b87052cba1b1a79c6c272e69
Is3ac <149327085+Is3ac@users.noreply.github.com>
2026-04-28 10:03:44 -0400

    Modify entrypoint.sh to start SQL Server correctly
    Updated entrypoint script to start SQL Server as mssql user.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -e
+
+# Fix permissions after volume mounts
 mkdir -p /.system /log /var/opt/mssql/secrets /var/opt/mssql/data /var/opt/mssql/log /var/opt/mssql/backup
 chown -R mssql:root /.system /log /var/opt/mssql
 chmod -R 775 /.system /log /var/opt/mssql
-exec /opt/mssql/bin/permissions_check.sh "$@"
+
+# Start SQL Server as mssql user
+exec gosu mssql /opt/mssql/bin/sqlservr
